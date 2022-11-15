bd57ac730c64d434efbc9d40e4740fb24845e457
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-11-15 01:20:20 +0100

    Initial commit

---
+++ src/Dan.EntityRegistryProxy/Models/SyncState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan.EntityRegistryProxy.Models;
+public class SyncState
+{
+    public DateTimeOffset LastUpdatedUnits { get; set; }
+    public DateTimeOffset LastUpdatedSubUnits { get; set; }
+}
